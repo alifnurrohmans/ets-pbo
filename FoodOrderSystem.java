053a3717e9aa8a2b966871bf54b33fdbf659aabc
alifnurrohmans <162116740+alifnurrohmans@users.noreply.github.com>
2024-10-24 15:21:13 +0700

    Update FoodOrderSystem.java

--- FoodOrderSystem.java
+++ FoodOrderSystem.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
 
+public class FoodOrderSystem {
+    private ArrayList<Order> orders;
+
+    public FoodOrderSystem() {
+        orders = new ArrayList<>();
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+}
